02a95173019729fb24c10207a0c72b3c5fc0e351
MarkusJx <28785953+markusjx@users.noreply.github.com>
2023-09-12 15:32:14 +0200

    fix: remove badly placed feature flag
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

--- datagen-rs/src/util/mod.rs
+++ datagen-rs/src/util/mod.rs
@@ -2,7 +2,7 @@ pub mod generate_error;
 pub mod helpers;
 #[cfg(feature = "plugin")]
 pub mod plugin_error;
+#[cfg(feature = "generate")]
 pub mod sequential_vec;
 pub mod traits;
-#[cfg(feature = "generate")]
 pub mod types;
