7f2bdcc6ce6e49c4baf10003e959e94a0e6490e9
MarkusJx <28785953+markusjx@users.noreply.github.com>
2023-09-24 17:31:45 +0200

    chore: copy readme before publish
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

---
+++ packages/cli/scripts/prePublish.ts
@@ -0,0 +1,17 @@
+import { readFileSync, writeFileSync, rmSync } from 'fs';
+
+const packageJsonPath = new URL('../package.json', import.meta.url);
+const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
+
+const version = packageJson.version;
+console.log(`Updating dependency versions to ${version}`);
+packageJson.devDependencies['@datagen-rs/types'] = version;
+packageJson.dependencies['@datagen-rs/node'] = version;
+
+writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+console.log('Copying README.md');
+const readmePath = new URL('../README.md', import.meta.url);
+const readme = readFileSync(readmePath, 'utf-8');
+rmSync(readmePath);
+writeFileSync(readmePath, readme, 'utf-8');
